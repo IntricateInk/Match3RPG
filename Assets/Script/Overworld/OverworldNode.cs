29139045c10a0ed5223f57d986a2d45928590217
Jason Julian <jason.julian27@gmail.com>
2018-06-08 16:33:51 +0800

    Fixedgit  tracking of metadata files, includes partial overworld commit

---
+++ Assets/Script/Overworld/OverworldNode.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverworldNode : MonoBehaviour {
+
+	private string encounterType; // link to encounter object
+    private string a;
+
+
+
+
+}
