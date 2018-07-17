b323790e91e0f8463de631c7fc2d08611ee8b52e
Jason Julian <jason.julian27@gmail.com>
2018-07-17 16:10:00 +0800

    added overworld map

--- Assets/Script/Overworld/OverworldManager.cs
+++ Assets/Script/Overworld/OverworldManager.cs
@@ -1,12 +1,30 @@
-﻿using System.Collections;
+﻿using Match3.Encounter;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class OverworldManager : MonoBehaviour
 {
     // singleton access
     private static OverworldManager _instance;
+    private OverworldMap _map { get; set; }
+    public GameObject loadingImage;
+    //public GameObject buttonPrefab;
+    private int score;
 
+    // constructor
+    void Start()
+    {
+        if (_map == null)
+        {
+            _map = createOverworld();
+        }
+    }
+
+
+    // singleton access
     public static OverworldManager Instance
     {
         get
@@ -19,16 +37,68 @@ public class OverworldManager : MonoBehaviour
         }
     }
 
+    // to keep level data across scene changes
     void Awake()
     {
-        DontDestroyOnLoad(gameObject);
+        loadingImage.SetActive(false);
+        DontDestroyOnLoad(this.gameObject);
     }
 
 
-    public static GameObject createOverworld()
+
+
+    public static OverworldMap createOverworld()
     {
+        OverworldMap map = new OverworldMap();
+        bool isGenerateSuccessful = map.generateLevel();
+        // show map
 
-        return null;
+        if (isGenerateSuccessful)
+        {
+           
+        }
+        
+        return map;
     }
+
+    OverworldNode.nodeType[,] GenerateLevelMap(int numberOfLevels = 10, int branches = 3)
+    {
+        List<string> overWorldNodeTypes = OverworldNode.FetchOverworldNodeTypes();
+        OverworldNode.nodeType[,] levelMap = new OverworldNode.nodeType[numberOfLevels, branches];
+
+        // By design, we are hardcoding the first and last few levels
+
+
+
+        for (int i = 1; i < numberOfLevels - 1; i++)
+        {
+
+        }
+
+        levelMap[numberOfLevels, 2] = OverworldNode.nodeType.BOSS;
+        return levelMap;
+    }
+
+
+    public void OnGUI()
+    {
+        for (int i = 0; i < _map.nodeList.Count; i++)
+        {
+            if (GUI.Button(new Rect(150 * i, 0, 100, 100), "Level" + i))
+            {
+                LoadScene(_map.nodeList[i]);
+            }
+
+        }
+        
+    }
+
+    public void LoadScene(OverworldNode node)
+    {
+        loadingImage.SetActive(true);
+        EncounterState.NewEncounter(node._encounterSheet);
+    }
+
 }
+
 
--- Assets/Script/Overworld/OverworldNode.cs
+++ Assets/Script/Overworld/OverworldNode.cs
@@ -1,13 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Match3.Encounter.Encounter;
 
-public class OverworldNode : MonoBehaviour {
-
-	private string encounterType; // link to encounter object
-    private string a;
+public class OverworldNode {
 
 
+    public EncounterSheet _encounterSheet { get; private set; } // link to encounter object
+    public enum nodeType {
+        MOB,
+        BOSS,
+        SHOP,
+        EVENT,
+
+    };
 
 
+    public nodeType _nodeType {get; private set;}
+    private bool obfuscated;
+   
+
+    // constructor
+    public OverworldNode (nodeType type)
+    {
+        this.obfuscated = Random.value < 0.3f ? true : false;
+        // randomizer
+        this._encounterSheet = EncounterSheet.TEST_1;
+    }
+
+
+    public static List<string> FetchOverworldNodeTypes()
+    {
+        string[] array = (string[])nodeType.GetValues(typeof(string));
+        List<string> list = new List<string>(array);
+        return list;
+    }
 }
