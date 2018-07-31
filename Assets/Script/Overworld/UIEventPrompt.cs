8279b0c2bbf9f1db3ca1a8e1c589610fd58bdbc8
Jason Julian <jason.julian27@gmail.com>
2018-07-31 14:27:00 +0800

    Added map to overworld scene

---
+++ Assets/Script/Overworld/UIEventPrompt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIEventPrompt : MonoBehaviour {
+
+    [SerializeField]
+    private Text title;
+
+    [SerializeField]
+    private Text storyText;
+
+    [SerializeField]
+    private GameObject buttonPrefab;
+    
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
